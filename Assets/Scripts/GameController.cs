de739739ae902ae5b482b587053bbda2d3ca1253
Aleksandr Kamaletdinov <a.kamaletdinov@corpwebgames.com>
2019-03-30 12:48:03 +0300

    restart button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
     private static GameController _instance;
     public static GameController Instance { get { return _instance; } }
 
-    public float PortalLives;
+    public int StartLives;
     public Text LivesText;
     public Text SecondsText;
     public GameObject _gameOver;
@@ -16,10 +17,20 @@ public class GameController : MonoBehaviour {
     private float _gameTimer = 0;
     static string LIVES = "Lives: ";
     static string TIMER = "Survived: ";
+    public Button Restart;
+    private int _currentLives;
 
     private void Start() {
         _instance = this;
+        Restart.onClick.AddListener(OnRestart);
+        Init();
+    }
+
+    private void Init() {
+        _currentLives = StartLives;
+        _gameTimer = 0f;
         _gameOver.SetActive(false);
+        IsGamOver = false;
     }
 
     private void Update() {
@@ -31,22 +42,31 @@ public class GameController : MonoBehaviour {
         if (IsGamOver)
             return;
 
-        if (PortalLives <= 0) {
+        if (_currentLives == 0) {
             _gameOver.SetActive(true);
             IsGamOver = true;
         }
     }
 
     public void Damage() {
-        PortalLives--;
-        if (PortalLives <=0) {
+        _currentLives--;
+        if (_currentLives <= 0) {
             IsGamOver = true;
             _gameOver.SetActive(true);
         }
     }
 
     private void UpdateUI() {
-        LivesText.text = LIVES + PortalLives;
+        LivesText.text = LIVES + _currentLives;
         SecondsText.text = TIMER + (int)_gameTimer;
+    }
+
+    private void OnRestart() {
+        Init();
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnDestroy() {
+        Restart.onClick.RemoveAllListeners();
     }
 }
